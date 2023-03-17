bbeab4b96b4200260f87949da4de69a2d609bf05
Jahid Eljah <jahideljah@gmail.com>
2023-03-17 22:20:18 +0100

    "Interface" & "Props" passing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["Berlin", "Frankfurt", "Maxico-City", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,6 +1,19 @@
-function ListGroup() {
-  let items = ["Berlin", "Frankfurt", "Maxico-City", "Paris"];
-  items = [];
+import { /* MouseEvent,*/ useState } from "react";
+
+interface ListGroupProps {
+  items: string[]; // an "array" of strings
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
+  // let items = ["Berlin", "Frankfurt", "Maxico-City", "Paris"];
+  // items = [];
+
+  // let selectedIndex = 0;
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  // const [toggle, setToggle] = useState(true);
 
   // const msg = items.length === 0 ? <h4>No item foung</h4> : null; =>>
 
@@ -8,14 +21,35 @@ function ListGroup() {
   //   return items.length === 0 ? <h4>No item foung</h4> : null;
   // };
 
+  // const eventHandler = (e: MouseEvent) => {
+  //   // e: MouseEvent // "type annotation"!
+  //   console.log("clicked", e); // console.log(item, "clicked", e);
+  // };
+
+  /*   const myStyle = {
+    color: "white",
+    backgroundColor: "red",
+    padding: "10px",
+    fontFamily: "Sans-Serif",
+  }; */
+
   return (
     <>
-      <h1>List</h1>
+      <h1> {heading} </h1>
       {/* {items.length === 0 ? <h4>No item foung</h4> : null} */}
       {items.length === 0 && <h4>No item foung</h4>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={index}>
+          <li
+            // className={`${toggle ? "active" : n ull}`}
+            className={selectedIndex === index ? "list-group-item active" : ""}
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+            // style={myStyle}
+          >
             <h4>{item}</h4>
           </li>
         ))}
