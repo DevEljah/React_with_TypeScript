9b3ca27285254de1a24323ffbb28fa83a9fd76e0
Jahid Eljah <jahideljah@gmail.com>
2023-04-06 21:26:54 +0200

    Fetching Data update

--- src/components/ProjectExpenseTracker/components/UserList.tsx
+++ src/components/ProjectExpenseTracker/components/UserList.tsx
@@ -1,14 +1,24 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
+interface User {
+  id: number;
+  name: string;
+}
+
 const UserList = () => {
-  const [users, setUsers] = useState<any[]>([]);
+  const [users, setUsers] = useState<User[]>([]);
+  const [firstUser, setfirstUser] = useState("");
 
   useEffect(() => {
     const getUser = async () => {
-      const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+      const res = await axios.get<User[]>(
+        "https://jsonplaceholder.typicode.com/users"
+      );
       const data = res.data;
+      const firstUser = res.data[0].name;
       setUsers(data);
+      setfirstUser(firstUser);
     };
     getUser();
   }, []);
@@ -16,6 +26,7 @@ const UserList = () => {
   return (
     <div>
       <h2>FetchData</h2>
+      <h4>first user's name: {firstUser}</h4>
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
