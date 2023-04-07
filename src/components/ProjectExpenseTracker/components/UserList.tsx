82905604b25de1cf765c8325140d7a8af698da1b
Jahid Eljah <jahideljah@gmail.com>
2023-04-07 13:30:24 +0200

    Cancelling a Fetch Request (cleanup func)

--- src/components/ProjectExpenseTracker/components/UserList.tsx
+++ src/components/ProjectExpenseTracker/components/UserList.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, CanceledError } from "axios";
 interface User {
   id: number;
   name: string;
 }
+
+const url = "https://jsonplaceholder.typicode.com/users";
 
 const UserList = () => {
   const [users, setUsers] = useState<User[]>([]);
@@ -11,20 +13,22 @@ const UserList = () => {
   const [error, setError] = useState("");
 
   useEffect(() => {
+    const controller = new AbortController();
+
     const getUser = async () => {
       try {
-        const res = await axios.get<User[]>(
-          "https://jsonplaceholder.typicode.com/users"
-        );
+        const res = await axios.get<User[]>(url, { signal: controller.signal });
         const data = res.data;
         const firstUser = res.data[0].name;
         setUsers(data);
         setFirstUser(firstUser);
       } catch (err) {
+        if (err instanceof CanceledError) return;
         setError((err as AxiosError).message);
       }
     };
     getUser();
+    return () => controller.abort();
   }, []);
 
   return (
@@ -47,3 +51,10 @@ const UserList = () => {
 };
 
 export default UserList;
+
+// Note: //
+// The "AbortController" is a way to cancel a request when the component unmounts or when a new request is made before the previous one has completed. By calling "controller.abort()" in the cleanup function, you cancel the ongoing request and prevent potential memory leaks.
+
+// Using the "signal" option with the "axios.get" method, which allows you to pass the "AbortSignal" object to the request. When the AbortSignal is aborted, the request will be canceled.
+
+// If the error is a "CanceledError", nothing is done, otherwise the error message is extracted and stored in the component's state for display in the UI.
